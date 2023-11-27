8db20ae811b20c5d66b34e67a9bb765fb713738a
Omar <ohawary98@gmail.com>
2023-11-27 21:55:27 +0200

    Added padding to the top of the Logo

--- lib/widgets/logo.dart
+++ lib/widgets/logo.dart
@@ -9,22 +9,25 @@ class Logo extends StatelessWidget{
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.topCenter,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'DrugDrop',
-            style: Theme.of(context).appBarTheme.titleTextStyle,
-          ),
-          const SizedBox(width: 5),
-          Transform.rotate(
-            angle: 30 * pi / 180,
-            child: const Icon(
-              MyFlutterApp.pills,
-              color: Color.fromRGBO(68, 191, 219, 1),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'DrugDrop',
+              style: Theme.of(context).appBarTheme.titleTextStyle,
             ),
-          ),
-        ],
+            const SizedBox(width: 5),
+            Transform.rotate(
+              angle: 30 * pi / 180,
+              child: const Icon(
+                MyFlutterApp.pills,
+                color: Color.fromRGBO(68, 191, 219, 1),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
