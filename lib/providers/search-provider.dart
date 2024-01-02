4e1f2af916ea0e75ba8c3f975c0ee7a6130de639
TaghreedSweideh <taghreedswidah9@gmail.com>
2024-01-02 11:09:12 +0300

    search and drug details

--- lib/providers/search-provider.dart
+++ lib/providers/search-provider.dart
@@ -1,10 +1,85 @@
-import '../models/category.dart';
+import '/providers/drug_data.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../main.dart';
+import '../providers/category.dart';
 
-class SearchItem with ChangeNotifier {
-  String? title;
-  String? imageUrl;
-  double? price;
-  Categories? cat;
-  SearchItem({this.imageUrl, this.title, this.price, this.cat});
+class SearchProvider with ChangeNotifier {
+  List<Categories> _searchedCategories = [];
+  List<Drug> _searchedDrugs = [];
+
+  List<Categories> get searchedCategories {
+    return [..._searchedCategories];
+  }
+
+  Categories findCatById(int id) {
+    return _searchedCategories.firstWhere((cat) => cat.id == id);
+  }
+
+  List<Drug> get searchedDrugs {
+    return [..._searchedDrugs];
+  }
+
+  Drug findDrugById(int id) {
+    return _searchedDrugs.firstWhere((drug) => drug.id == id);
+  }
+
+  Future<void> getResult(String type, String value, String cat) async {
+    print('getting results');
+    var url;
+    print('$type and $value');
+    if (type == 'Medicine') {
+      url = Uri.http(host, '/api/search/drug',
+          {'lang_code': 'en', 'search': value, 'category': cat});
+    } else if (type == 'Category') {
+      url = Uri.http(host, '/api/serach/category', {'search': value});
+    }
+
+    try {
+      final response = await http.get(url, headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token'
+      });
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final List<Categories> loadedCategories = [];
+      List<Drug> loadedDrugs = [];
+      if (extractedData == Null) {
+        return;
+      }
+      if (type == 'Category') {
+        extractedData['Data'].forEach((index) {
+          loadedCategories.add(Categories(
+            id: index['id'],
+            name: index['en_name'],
+          ));
+        });
+        _searchedCategories = loadedCategories;
+      } else if (type == 'Medicine') {
+        extractedData['Data'].forEach((drug) {
+          loadedDrugs.add(
+            Drug(
+                id: drug['id'],
+                tradeName: drug['trade_name'],
+                scientificName: drug['scientific_name'],
+                company: drug['company'],
+                tagId: drug['tag_id'],
+                dose: drug['dose'],
+                doseUnit: drug['dose_unit'],
+                price: drug['price'],
+                quantity: drug['quantitiy'],
+                expiryDate: drug['expiry_date'].toString(),
+                imageUrl: drug['img_url'] ?? 'null'),
+          );
+        });
+        _searchedDrugs = loadedDrugs;
+      } else {
+        print('There is nothing to be fetched');
+      }
+      notifyListeners();
+    } catch (error) {
+      print(error.toString());
+      //throw (error);
+    }
+  }
 }
--- lib/screens/search-screen.dart
+++ lib/screens/search-screen.dart
@@ -1,5 +1,10 @@
-import '../providers/search-provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/search-provider.dart';
+
+import '/screens/drug-details-screen.dart';
+import '/widgets/category_item.dart';
 
 class SearchScreen extends StatefulWidget {
   static const routeName = '/search-screen';
@@ -8,48 +13,115 @@ class SearchScreen extends StatefulWidget {
   State<SearchScreen> createState() => _SearchScreenState();
 }
 
+final Map<String, String> searchData = {
+  'type': '',
+  'value': '',
+  'category': '',
+};
+
 class _SearchScreenState extends State<SearchScreen> {
-  static List<SearchItem> medicines = [
-    SearchItem(
-        imageUrl: 'assets/images/Medical prescription-rafiki.png',
-        price: 23,
-        title: 'taghreed'),
-    SearchItem(
-        imageUrl: 'assets/images/Medical prescription-rafiki.png',
-        price: 23,
-        title: 'taghreed'),
-    SearchItem(
-        imageUrl: 'assets/images/Medical prescription-rafiki.png',
-        price: 23,
-        title: 'Nour')
+  List<String> searching = [
+    'Category',
+    'Medicine',
   ];
-  List<SearchItem> filteredMedicines = List.from(medicines);
-  void updateList(String value) {
-    setState(() {
-      filteredMedicines = medicines
-          .where((medTitle) =>
-              medTitle.title!.toLowerCase().contains(value.toLowerCase()))
-          .toList();
-    });
+  var _isLoading = false;
+  void _showDialog(BuildContext context, String text) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        title: Center(
+          child: Text(
+            text,
+            style: TextStyle(color: Theme.of(context).colorScheme.background),
+          ),
+        ),
+        content: TextFormField(
+          textInputAction: TextInputAction.done,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+              fontFamily: 'poppins',
+              color: Theme.of(context).colorScheme.primary),
+          showCursor: true,
+          decoration: InputDecoration(
+            isDense: true,
+            contentPadding: EdgeInsets.all(10),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
+              borderSide: BorderSide(
+                  width: 0.8, color: Theme.of(context).colorScheme.primary),
+            ),
+          ),
+          onChanged: (text) {
+            searchData['category'] = text.toString();
+          },
+        ),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  updateData(searchData['type']!, searchData['value']!,
+                      searchData['category']!);
+                },
+                child: const Text('search'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  void updateData(String type, String value, String cat) async {
+    if (type == '') {
+      print('can\'t search you must choose the type of search');
+    }
+
+    setState(() => _isLoading = true);
+    try {
+      //here I will get the result of search
+      Provider.of<SearchProvider>(context, listen: false)
+          .getResult(type, value, cat)
+          .then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    } catch (error) {
+      print(error);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context).colorScheme;
     var media = MediaQuery.of(context).size;
+    final searchResult = Provider.of<SearchProvider>(context);
+    var categories = searchResult.searchedCategories;
+    var medicines = searchResult.searchedDrugs;
     return Column(
       children: [
         Padding(
           padding: EdgeInsets.symmetric(
             horizontal: media.width * 0.03,
-            vertical: media.height * 0.03,
+            vertical: media.height * 0.01,
           ),
-          child: TextField(
-            onChanged: (value) => updateList(value),
+          child: TextFormField(
+            textInputAction: TextInputAction.done,
+            keyboardType: TextInputType.text,
             style: TextStyle(fontFamily: 'poppins', color: theme.primary),
             showCursor: true,
             decoration: InputDecoration(
+              suffixIcon: IconButton(
+                  icon: Icon(Icons.filter_list_alt),
+                  onPressed: () {
+                    _showDialog(context, 'search for category');
+                  }),
               isDense: true,
+              filled: true,
+              fillColor: Colors.transparent,
               contentPadding: EdgeInsets.all(10),
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(30.0),
@@ -62,46 +134,99 @@ class _SearchScreenState extends State<SearchScreen> {
               ),
               prefixIconColor: theme.primary,
             ),
+            onChanged: (text) {
+              searchData['value'] = text.toString();
+              updateData(searchData['type']!, searchData['value']!,
+                  searchData['category']!);
+            },
           ),
         ),
-        SizedBox(
-          height: media.height * 0.02,
-        ),
-        Expanded(
-            child: filteredMedicines.length == 0
-                ? Center(
-                    child: Text('No Such Medicine'),
-                  )
-                : ListView.builder(
-                    itemCount: medicines.length,
-                    itemBuilder: (context, index) => Column(
-                      children: [
-                        ListTile(
-                          title: Text(
-                            medicines[index].title!,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: theme.primary,
-                            ),
-                          ),
-                          subtitle: Text(
-                            '${medicines[index].price}',
-                            style: TextStyle(
-                              color: theme.primary,
-                            ),
-                          ),
-                          trailing: Text(
-                            '${medicines[index].price}',
-                            style: TextStyle(
-                              color: Colors.amber,
-                            ),
-                          ),
-                          leading: Image.asset(medicines[index].imageUrl!),
-                        ),
-                        Divider()
-                      ],
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: searching
+                .map(
+                  (category) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: FilterChip(
+                      selectedColor: theme.background.withOpacity(0.4),
+                      backgroundColor: Colors.blueGrey.withOpacity(0.2),
+                      label: Text(category),
+                      selected: searchData['type']!.contains(category),
+                      onSelected: (selected) {
+                        setState(() {
+                          if (selected) {
+                            searchData['type'] = category;
+                          } else {
+                            searchData['type'] = '';
+                          }
+                        });
+                      },
                     ),
-                  ))
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+        SizedBox(height: media.height * 0.02),
+        Expanded(
+          child: _isLoading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : (medicines.length == 0 && categories.length == 0)
+                  ? Center(
+                      child: Text('No Such Item'),
+                    )
+                  : searchData['type'] == 'Category'
+                      ? ListView.builder(
+                          itemCount: categories.length,
+                          itemBuilder: (_, i) => Column(
+                            children: [
+                              CategroyItem(
+                                categories[i].id,
+                                categories[i].name,
+                              ),
+                            ],
+                          ),
+                        )
+                      : ListView.builder(
+                          itemCount: medicines.length,
+                          itemBuilder: (context, index) => Column(
+                            children: [
+                              ListTile(
+                                title: Text(
+                                  medicines[index].tradeName!,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: theme.primary,
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  '${medicines[index].price}',
+                                  style: TextStyle(
+                                    color: theme.primary,
+                                  ),
+                                ),
+                                leading: medicines[index].imageUrl == 'null'
+                                    ? Image.asset(
+                                        'assets/images/Medical prescription-rafiki.png')
+                                    : Image.network(
+                                        medicines[index].imageUrl!,
+                                        fit: BoxFit.cover,
+                                      ),
+                                onTap: () {
+                                  Navigator.of(context).pushNamed(
+                                    DrugDetailsScreen.routeName,
+                                    arguments: medicines[index].id,
+                                  );
+                                },
+                              ),
+                              Divider()
+                            ],
+                          ),
+                        ),
+        )
       ],
     );
   }
